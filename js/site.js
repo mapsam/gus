72322d08181cd8d8e1db8d635e49bccc93e19bb9
svmatthews <matthews.sam@gmail.com>
2014-09-09 12:18:40 -0700

    image and live google sheet

--- js/site.js
+++ js/site.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     key: '1SU4q9l75X20wbvIecroglqIJYyMXn5n6Tnx6lRG5E_A',
     callback: function(data, tabletop) { 
       console.log(data);
-      var map = L.map('map').setView([45.96642454131025, -113.99414062499999],7);
+      var map = L.map('map').setView([46.66451741754235, -114.95544433593749],8);
       L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png').addTo(map);
       for(i=0;i<data.length;i++) {
         var marker = L.marker([parseFloat(data[i].lat), parseFloat(data[i].lng)]).addTo(map);

28cf852da2bf5c53aaa2e57a5210be39ba1d1fbb
svmatthews <matthews.sam@gmail.com>
2014-09-11 00:46:55 -0700

    web UI for showing points from google spreadsheet

--- js/site.js
+++ js/site.js
@@ -1,20 +1,33 @@
 $(document).ready(function(){
-  Tabletop.init( { 
-    key: '1SU4q9l75X20wbvIecroglqIJYyMXn5n6Tnx6lRG5E_A',
-    callback: function(data, tabletop) { 
-      console.log(data);
-      var map = L.map('map').setView([45.96642454131025, -113.99414062499999],7);
-      L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png').addTo(map);
-      for(i=0;i<data.length;i++) {
-        var marker = L.marker([parseFloat(data[i].lat), parseFloat(data[i].lng)]).addTo(map);
-        marker.bindPopup('<strong>'+data[i].notes+'</strong><br><em>Posted By: '+data[i].entryby);
-        console.log(marker);
-      }
 
-      map.on('contextmenu', function(e) {
-        alert("Lat: " + e.latlng.lat + ", Lng: " + e.latlng.lng)
-      });
-    },
-   simpleSheet: true } );
+  $('#submit').on('click', function(){
+    mapIt($('#key').val());
+  });
+  
+  function mapIt(key){
+    Tabletop.init( { 
+      key: key,
+      callback: function(data, tabletop) { 
+        var map = L.map('map').setView([0,0],7);
+        var points = L.layerGroup();
+        L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png').addTo(map);
+
+        for(i=0;i<data.length;i++) {
+          var marker = L.marker([parseFloat(data[i].lat), parseFloat(data[i].lng)]);
+          marker.bindPopup('<strong>'+data[i].notes+'</strong><br><em>Posted By: '+data[i].entryby);
+          points.addLayer(marker);
+        }
+
+        var overlayMaps = {
+          "Points": points
+        };
+
+        L.control.layers(false, overlayMaps).addTo(map);
+
+        // map.fitBounds(map.getBounds(points));
+      },
+     simpleSheet: true } );
+  }
+  
 
 });
